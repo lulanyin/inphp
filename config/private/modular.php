3c763fc75d28df2b91874f23a6a82b5db16cef9f
Hunter <lulanyin@qq.com>
2020-12-01 23:52:49 +0800

    模块化初定义...

--- config/private/modular.php
+++ config/private/modular.php
@@ -2,28 +2,17 @@
 /**
  * Create By Hunter
  * 2020/12/1 7:05 下午
- * 路由将使用模块化
+ * 路由将使用模块化，不再区分http和ws，模块里边可以允许存在两种类型的服务控制器逻辑
+ * http 和 ws 服务都将使用以下的路由配置
  */
 return [
-    //HTTP
-    'http'  => [
-        //入口
-        'home'  => 'app\modules\\',
-        //默认
-        'default' => 'inphp',
-        //列表
-        'list'  => [
-            //inphp 官方模块，带有
-            'inphp' => [
-                //文件夹
-                'path'   => 'inphp',
-                //指定配置文件，
-                'config' => ''
-            ]
-
-        ]
-    ],
-    'ws'    => [
-
+    //入口
+    'home'  => 'app\modules\\',
+    //默认
+    'default' => 'inphp',
+    //列表
+    'list'  => [
+        //inphp 官方模块，带有
+        'inphp' => "inphp"
     ]
 ];
