6a3502afd5163a82ea4635f4bf18d3489c43f3e6
lanyin <me@lanyin.lu>
2020-12-13 23:47:44 +0800

    系统模块开发中...

---
+++ public/assets/framework/js/web.js
@@ -0,0 +1,12 @@
+function updateUI(){
+    $('.ui.dropdown').dropdown();
+    $('.ui.accordion').accordion();
+    $('.ui.checkbox,.ui.radio').checkbox();
+}
+
+$(function (){
+
+    //加载UI
+    updateUI();
+
+});
